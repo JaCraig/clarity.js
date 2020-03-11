9417823efcdfaf9c581c1684048a26680b79fcf2
James Craig <craig.jamesa@gmail.com>
2020-03-10 23:10:22 -0400

    Package updates.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 import serve from 'rollup-plugin-serve';
