d728c00fa34b1c466f6b00dfe49abf9fa0519740
James Craig <craig.jamesa@gmail.com>
2021-11-24 11:11:29 -0500

    -  Finalizing page hopefully.

--- Clarity.cjs.js
+++ Clarity.cjs.js
@@ -6,8 +6,6 @@
  * Author: James Craig
  */
 
-
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
--- Clarity.esm.js
+++ Clarity.esm.js
@@ -6,8 +6,6 @@
  * Author: James Craig
  */
 
-
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 import Vue, { openBlock, createElementBlock, normalizeClass, createElementVNode, renderSlot, createTextVNode, Fragment, renderList, withModifiers, toDisplayString, withDirectives, vModelText, createCommentVNode, vModelSelect, vModelDynamic, createStaticVNode, vShow, createBlock, resolveDynamicComponent, resolveComponent, createVNode, withCtx, Transition } from 'vue';
 import moment from 'moment';
 import showdown from 'showdown';
