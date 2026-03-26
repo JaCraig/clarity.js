d915dac6d9e503abed0a02939de89032176989d8
James Craig <craig.jamesa@gmail.com>
2026-03-25 20:56:45 -0400

    fix(dependencies): update TypeScript and improve Rollup configuration
    Updated TypeScript to version 6.0.2 and made various improvements to the Rollup configuration for better build management and compatibility.
    
    - Upgraded TypeScript from 5.9.2 to 6.0.2 in package.json.
    - Refactored Rollup configuration for better readability and organization.
    - Enhanced TypeScript plugin configuration in Rollup to include specific file types.
    - Added rootDir and ignoreDeprecations options in tsconfig.json for better TypeScript management.
    - Maintained existing build processes for HTML, images, and CSS files.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,112 +1,118 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json' with { type: 'json' };
-import serve from 'rollup-plugin-serve';
-import license from 'rollup-plugin-license';
-import path from 'path';
-import postcss from 'rollup-plugin-postcss';
-import autoprefixer from 'autoprefixer';
-import VuePlugin from 'rollup-plugin-vue';
-import multi from '@rollup/plugin-multi-entry';
-import livereload from 'rollup-plugin-livereload';
-import alias from '@rollup/plugin-alias';
-import replace from '@rollup/plugin-replace';
-import copy from './build/rollup-plugin-copy.mjs';
+import alias from "@rollup/plugin-alias";
+import commonjs from "@rollup/plugin-commonjs";
+import multi from "@rollup/plugin-multi-entry";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import autoprefixer from "autoprefixer";
+import path from "path";
+import license from "rollup-plugin-license";
+import livereload from "rollup-plugin-livereload";
+import postcss from "rollup-plugin-postcss";
+import serve from "rollup-plugin-serve";
+import typescript from "rollup-plugin-typescript2";
+import VuePlugin from "rollup-plugin-vue";
+import copy from "./build/rollup-plugin-copy.mjs";
+import pkg from "./package.json" with { type: "json" };
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const external = Object.keys(pkg.dependencies);
 const isProduction = !process.env.ROLLUP_WATCH;
-const globals = { vue: 'Vue', moment: 'moment', showdown: 'showdown' };
-
+const globals = { vue: "Vue", moment: "moment", showdown: "showdown" };
 
 export default [
-	// HTML files
-	{
-		input: "src/temp.js",
-		output: {
-			file: "out/temp.js",
-			format: "cjs",
-		},
-		plugins: [
-			copy({ assets: [{ source: "index.html", destination: "./index.html" },{ source: "index2.html", destination: "./index2.html" }] }),
-		],
-	},
-	// Image files
-	{
-		input: "src/images/temp.js",
-		output: {
-			file: "out/images/temp.js",
-			format: "cjs",
-		},
-		plugins: [
-			copy({ assets: [{ source: "./", destination: "./" }] }),
-		],
-	},
-	// CSS files
-	{
-		input: 'src/less/main.less',
-		output: [{ file: 'dist/Clarity.css' },],
-		plugins: [
-			postcss({ extract: true, plugins: [autoprefixer()] }),
-		]
-	},
-	// CSS files min
-	{
-		input: 'src/less/main.less',
-		output: [{ file: 'dist/Clarity.min.css' },],
-		plugins: [
-			postcss({ extract: true, plugins: [autoprefixer()], minimize: true, sourceMap: true }),
-			copy({ assets: [{ source:'../../node_modules/@fortawesome/fontawesome-free/webfonts', destination:"./webfonts" }] })
-		]
-	},
-	// Node and package system builds
-	{
-		input: [
-			'src/ts/Extensions/HTMLElement.ts',
-			'src/ts/Extensions/NodeList.ts',
-			'src/ts/Extensions/Object.ts',
-			'src/ts/Extensions/String.ts',
-			'src/ts/Clarity.ts',
-		],
-		external: external,
-		plugins: [
-			resolve(),
-			typescript(),
-			VuePlugin(),
-			commonjs({ extensions: ['.js','.ts', '.vue']}),
-			multi(),
-			alias({
-				entries: [
-				  { find: 'vue', replacement: 'vue/dist/vue.js' },
-				]
-			}),
-			replace({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')}),
-			license({
-				banner: {
-					content: {
-						file: path.join(__dirname, 'LICENSE-CODE'),
-					},
-				},
-				thirdParty: {
-					allow: '(MIT OR Apache-2.0 OR 0BSD)',
-				},
-			}),
-			!isProduction && serve({ contentBase: ['out','dist'] }),
-			!isProduction && livereload(),
-		],
-		output: [
-			{ file: pkg.main, format: 'cjs', globals: globals },
-			{ file: pkg.module, format: 'es', globals: globals  },
-			{ file: 'dist/Clarity.cjs.min.js', format: 'cjs', globals: globals, plugins: []  },
-			{ file: 'dist/Clarity.esm.min.js', format: 'es', globals: globals, plugins: []  },
-			{ name: 'clarity', file: pkg.browser, format: 'umd', globals: globals },
-			{ name: 'clarity', file: 'dist/Clarity.umd.min.js', format: 'umd', globals: globals, plugins: [] }
-		]
-	},
+    // HTML files
+    {
+        input: "src/temp.js",
+        output: {
+            file: "out/temp.js",
+            format: "cjs",
+        },
+        plugins: [
+            copy({
+                assets: [
+                    { source: "index.html", destination: "./index.html" },
+                    { source: "index2.html", destination: "./index2.html" },
+                ],
+            }),
+        ],
+    },
+    // Image files
+    {
+        input: "src/images/temp.js",
+        output: {
+            file: "out/images/temp.js",
+            format: "cjs",
+        },
+        plugins: [copy({ assets: [{ source: "./", destination: "./" }] })],
+    },
+    // CSS files
+    {
+        input: "src/less/main.less",
+        output: [{ file: "dist/Clarity.css" }],
+        plugins: [postcss({ extract: true, plugins: [autoprefixer()] })],
+    },
+    // CSS files min
+    {
+        input: "src/less/main.less",
+        output: [{ file: "dist/Clarity.min.css" }],
+        plugins: [
+            postcss({ extract: true, plugins: [autoprefixer()], minimize: true, sourceMap: true }),
+            copy({
+                assets: [
+                    { source: "../../node_modules/@fortawesome/fontawesome-free/webfonts", destination: "./webfonts" },
+                ],
+            }),
+        ],
+    },
+    // Node and package system builds
+    {
+        input: [
+            "src/ts/Extensions/HTMLElement.ts",
+            "src/ts/Extensions/NodeList.ts",
+            "src/ts/Extensions/Object.ts",
+            "src/ts/Extensions/String.ts",
+            "src/ts/Clarity.ts",
+        ],
+        external: external,
+        plugins: [
+            resolve(),
+            VuePlugin(),
+            typescript({
+                check: false,
+                tsconfig: "./tsconfig.json",
+                include: [/\.ts$/, /\.tsx$/, /\.vue\?vue&type=script.*lang\.ts$/],
+            }),
+            commonjs({ extensions: [".js", ".cjs"] }),
+            multi(),
+            alias({
+                entries: [{ find: "vue", replacement: "vue/dist/vue.js" }],
+            }),
+            replace({ "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "production") }),
+            license({
+                banner: {
+                    content: {
+                        file: path.join(__dirname, "LICENSE-CODE"),
+                    },
+                },
+                thirdParty: {
+                    allow: "(MIT OR Apache-2.0 OR 0BSD)",
+                },
+            }),
+            !isProduction && serve({ contentBase: ["out", "dist"] }),
+            !isProduction && livereload(),
+        ],
+        output: [
+            { file: pkg.main, format: "cjs", globals: globals },
+            { file: pkg.module, format: "es", globals: globals },
+            { file: "dist/Clarity.cjs.min.js", format: "cjs", globals: globals, plugins: [] },
+            { file: "dist/Clarity.esm.min.js", format: "es", globals: globals, plugins: [] },
+            { name: "clarity", file: pkg.browser, format: "umd", globals: globals },
+            { name: "clarity", file: "dist/Clarity.umd.min.js", format: "umd", globals: globals, plugins: [] },
+        ],
+    },
 ];
